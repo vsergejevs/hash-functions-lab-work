4e9234c716224ae96a7b6e0aef5022eb3d5cc8d0
Valeriy Sergeyev <valeriy.sergeyev7@gmail.com>
2019-09-12 20:55:48 +0100

    Update main.cpp
    Adding first array and pushing into first random number

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-// TODO: generate random integer number from 100 - 1000
+//v1 = rand() % 100;         // v1 in the range 0 to 99
+//v2 = rand() % 100 + 1;     // v2 in the range 1 to 100
+//v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014
+//v4 = rand() % 1001 + 101;   // v3 in the range 1985-2014
 
 // arrays example
 #include <iostream>
@@ -6,23 +9,24 @@
 #include <time.h>       /* time */
 using namespace std;
 
+// TODO: declare empty array which will hold the random numbers - DONE
+int firstArray [1000];
 
 int main ()
 {
-  int firstRand, secondRand;
-  /* initialize random seed: */
-  srand (time(NULL));
+  int secondRand;
 
-  /* generate secret number between 1 and 10: */
-  firstRand = rand() % 10 + 1;
-  secondRand = rand() % 1001 + 101;   // v3 in the range 1985-2014
-  
- // v1 = rand() % 100;         // v1 in the range 0 to 99
-  //v2 = rand() % 100 + 1;     // v2 in the range 1 to 100
-  //v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014
-  //v4 = rand() % 1001 + 101;   // v3 in the range 1985-2014
-  
-  cout << secondRand;
+  // TODO: generate random integer number from 100 - 1000 - DONE
+  /* initialize random seed: */
+  srand (time(nullptr));
+
+  secondRand = rand() % 900 + 100;   // v3 in the range 100-1000
+
+  // TODO: push random generated number into array
+  firstArray[0] = secondRand;
+
+  cout << "this is a random number ranging from (100 - 1000) - " << secondRand << endl;
+  cout << "this is the array holding random numbers: " << "[" << firstArray[0] << "]" << endl;
   
   return 0;
 } 
