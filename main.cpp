29bb9c106f371627544e477f36d3669b23406298
Valeriy Sergeyev <valeriy.sergeyev7@gmail.com>
2019-09-12 22:24:49 +0100

    Update main.cpp
    Added for loop to generate random numbers and push them to array

--- main.cpp
+++ main.cpp
@@ -14,19 +14,26 @@ int firstArray [1000];
 
 int main ()
 {
-  int secondRand;
+  int firstRand, secondRand;
 
   // TODO: generate random integer number from 100 - 1000 - DONE
   /* initialize random seed: */
   srand (time(nullptr));
-
-  secondRand = rand() % 900 + 100;   // v3 in the range 100-1000
+  firstRand = rand() % 900 + 100;    // v1 in the range 100-1000
+  secondRand = rand() % 900 + 100;   // v2 in the range 100-1000
 
   // TODO: push random generated number into array
-  firstArray[0] = secondRand;
+//  firstArray[0] = secondRand;
+
+  // TODO: create a for loop to generate random numbers and push them to array
+  int i;
+  for (i = 0; i < secondRand; i++)
+  {
+      firstArray[i] = firstRand;
+  }
 
   cout << "this is a random number ranging from (100 - 1000) - " << secondRand << endl;
-  cout << "this is the array holding random numbers: " << "[" << firstArray[0] << "]" << endl;
+  cout << "this is the array holding random numbers: " << "[" << firstArray[6] << "]" << endl;
   
   return 0;
 } 
